02190326c0e821c4e9ab9b46fb49c907774cbf68
myx2024 <3426980380@qq.com>
2024-08-06 13:51:33 +0800

    注释更新

--- examples/system/freertos/basic_freertos_smp_usage/main/basic_freertos_smp_usage.c
+++ examples/system/freertos/basic_freertos_smp_usage/main/basic_freertos_smp_usage.c
@@ -97,11 +97,11 @@ static void config_console(void)
     esp_console_register_help_command();//注册一个帮助命令，允许用户通过输入 help 命令来获取帮助信息
 
     // register entry functions for each component 为每个组件注册入口函数
-    register_creating_task();
-    register_queue();
-    register_lock();
-    register_task_notification();
-    register_batch_proc_example();
+    register_creating_task();       //运行演示如何创建和运行固定和未固定任务的示例
+    register_queue();               //运行演示如何使用队列在任务之间通信的示例
+    register_lock();                //运行演示如何使用互斥锁和自旋锁来保护共享资源的示例
+    register_task_notification();   //运行演示如何使用任务通知同步任务的示例
+    register_batch_proc_example();  //运行将队列、互斥锁、任务通知组合在一起的示例
 
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
     printf("\n"
@@ -110,5 +110,5 @@ static void config_console(void)
 
 void app_main(void)
 {
-    config_console();
+    config_console();//初始化和配置控制台 REPL 环境，允许用户通过命令行与系统交互
 }
